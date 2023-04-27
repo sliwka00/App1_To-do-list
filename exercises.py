535f038c7b4f8f7b46cce74e17c44901ea41b2e2
sliwk <sliwa.pawel@interia.pl>
2023-04-27 13:10:08 +0200

    Create basic GUI without interactions, day 16

--- exercises.py
+++
@@ -1,6 +0,0 @@
-[{"question_text":"Ktory pilkarz zdobyl mistrzostwo Polski z Gornikiem?",
-  "alternatives":["Roman Lentner","Igor Angulo","Marcin Brosz","Tomasz Prasnal"],
-  "correct_answer":1}]
-[{"question_text":"W ktorym roku Gornik zdobyl mistrzostwo?",
-  "alternatives":["1948","2016","1968","1888"],
-  "correct_answer":4}]
---
+++ gui.py
@@ -0,0 +1,11 @@
+import PySimpleGUI as sg
+
+label=sg.Text("Type in a to-do")
+input_box=sg.InputText(tooltip="Enter todo")
+button=sg.Button("Add")
+
+window=sg.Window("My To-do App", layout=[[label],[input_box],[button]])
+
+window.read()
+window.close()
+
