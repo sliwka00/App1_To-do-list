c373ad7c7fba601b9f57edb5529989f5d151bd3b
sliwk <sliwa.pawel@interia.pl>
2023-05-19 23:21:50 +0200

    Implement "Complete" and "Exit" Button

--- gui.py
+++ gui.py
@@ -7,14 +7,19 @@ button=sg.Button("Add")
 list_box=sg.Listbox(values=functions.get_todos(), size=(45, 10),
                     key='todos', enable_events=True)
 edit_button=sg.Button("Edit")
+complete_button=sg.Button("Complete")
+exit_button=sg.Button("Exit")
 
 window=sg.Window("My To-do App",
-                 layout=[[label], [input_box,button], [list_box, edit_button]],
+                 layout=[[label],
+                         [input_box,button],
+                         [list_box, edit_button,complete_button],
+                         [exit_button]],
                  font=('Helvetica',20))
 
 while True:
     event,values=window.read()
-    print(event)
+    print(event)     # po kliknięciu w Button event to jego nazwa, dzięki temu można zrobić poniżej case , reagujace na klikniecie
     print(values)
     match event:
         case "Add":
@@ -31,6 +36,15 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])   #dzięki temu po kliknieciu na zadanie do edycji pojawia(odświeża) się w inputboxie
+        case 'Complete':
+            todos = functions.get_todos()
+            todo_number = list_box.get_indexes()[0]
+            todos.pop(todo_number)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='') #po usunięciu pozycji z listy "czyścimy" inputbox, żeby był pusty
+        case 'Exit':
+            break
         case sg.WIN_CLOSED: # co się stanie po zamknięciu okna gui
             break
 window.close()
